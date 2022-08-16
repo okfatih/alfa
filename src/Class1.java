4baf9f6b339eda816b59e895e7587bac81d3d78a
Fatih <fatih_ok4@hotmail.com>
2022-08-16 19:56:28 +0200

    v2

---
+++ src/Class1.java
@@ -0,0 +1,20 @@
+public class Class1 {
+    /*
+            /*
+        Git init bir kereye mahsus kullanırız local repo oluşturmak
+        için .git ile klasörümüzün içindeki dosyalarımızı illişkilendirmek
+        için kullanılır
+        2) git add . working space den  yerelden dosyalarımı staging areayı
+        commitlemek için beklenen yere gönderir
+        3) git commit -m "mesaj" staging area dan commit store a dosyalarımı
+        göndermek için kullanılır
+        commit yani versiyon oluşturmuş olurum
+        git log --oneline en son yaptığım commiti gösterir
+        4) git push remote yani uzak repo olan githuba  göndermek için kullandığımız kod
+        yanlız git push komutunu direkt kullanmak istersek 1 kez
+        git remote add origin -address bu adres github repost oluşturuduğumuz adres
+        git push -u origin master
+        Not: bu iki komutu tek seferde kullandıktan sonra diğer ikinci commitlerim için
+        sadece git push ykullanırım
+         */
+}
